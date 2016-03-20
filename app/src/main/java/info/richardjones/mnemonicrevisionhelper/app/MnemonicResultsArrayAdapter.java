5c82955dd4f5cc04472deb87e8cd40494bc7e68b
iamrichardjones <iamrichardjones@gmail.com>
2016-03-20 13:53:06 +0000

    remove unused code

--- app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/MnemonicResultsArrayAdapter.java
+++ app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/MnemonicResultsArrayAdapter.java
@@ -19,17 +19,11 @@ import java.util.List;
 public class MnemonicResultsArrayAdapter extends ArrayAdapter<MatchingMnemonic> implements Filterable {
 
     private final Context context;
-    private final LayoutInflater inflater;
     private final MnemonicFilter filter;
-//    private List<MatchingMnemonic> originalData;
-//    private List<MatchingMnemonic> filteredData;
 
     public MnemonicResultsArrayAdapter(Context context, List<MatchingMnemonic> values) {
         super(context, -1, values);
         this.context = context;
-//        this.originalData = values;
-//        this.filteredData = values;
-        inflater = LayoutInflater.from(context);
         filter = new MnemonicFilter(this, values);
 
     }
@@ -66,48 +60,5 @@ public class MnemonicResultsArrayAdapter extends ArrayAdapter<MatchingMnemonic> 
     @Override
     public Filter getFilter() {
         return filter;
-//        Filter filter = new Filter() {
-//
-//            @SuppressWarnings("unchecked")
-//            @Override
-//            protected void publishResults(CharSequence constraint, FilterResults results) {
-//                if (results.count == 0) {
-//                    notifyDataSetInvalidated();
-//                } else {
-//                    filteredData = (List<MatchingMnemonic>) results.values;
-//                    notifyDataSetChanged();
-//                }
-//            }
-//
-//            @Override
-//            protected FilterResults performFiltering(CharSequence constraint) {
-//                FilterResults results = new FilterResults();
-//                if (constraint.toString().equals("") || constraint.length() == 1) {
-//                    results.values = originalData;
-//                    results.count = originalData.size();
-//                    return results;
-//                }
-//
-//                List<MatchingMnemonic> filteredArrayNames = new ArrayList<MatchingMnemonic>();
-//
-//                for (int i = 0; i < originalData.size(); i++) {
-//                    MatchingMnemonic dataNames = originalData.get(i);
-//
-//                    char[] charArray = constraint.toString().toUpperCase().toCharArray();
-//                    Arrays.sort(charArray);
-//
-//                    if (dataNames.getOrderedMnemonic().toUpperCase().equals(new String(charArray).toUpperCase()))  {
-//                        filteredArrayNames.add(dataNames);
-//                    }
-//                }
-//
-//                results.count = filteredArrayNames.size();
-//                results.values = filteredArrayNames;
-//
-//                return results;
-//            }
-//        };
-//
-//        return filter;
     }
 }
