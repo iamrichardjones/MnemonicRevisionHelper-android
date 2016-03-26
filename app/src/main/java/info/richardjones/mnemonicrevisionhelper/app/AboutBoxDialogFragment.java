e2a1ee4931ac84efcf5351ba44921a59978166d8
iamrichardjones <iamrichardjones@gmail.com>
2016-03-26 13:21:08 +0000

    Fix versions

--- app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/AboutBoxDialogFragment.java
+++ app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/AboutBoxDialogFragment.java
@@ -19,7 +19,6 @@ public class AboutBoxDialogFragment extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder
-//                .setMessage(FORMAT.format(new Date()))
                 .setMessage(R.string.dialog_about)
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
