b5ca272107e879cc0fcdb91ad8aef2ff9e2b29bb
iamrichardjones <iamrichardjones@gmail.com>
2016-03-29 18:51:34 +0100

    Add preference so that results are shown for partial matches.

--- app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/MnemonicFilter.java
+++ app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/MnemonicFilter.java
@@ -2,16 +2,13 @@ package info.richardjones.mnemonicrevisionhelper.app;
 
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import info.richardjones.mnemonicrevisionhelper.app.loader.MatchingMnemonic;
 import info.richardjones.mnemonicrevisionhelper.app.loader.MnemonicLoader;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class MnemonicFilter extends Filter {
 
@@ -45,11 +42,16 @@ public class MnemonicFilter extends Filter {
         Boolean incSlogans = sharedPref.getBoolean(SettingsActivity.USE_SLOGAN_PREFERENCES, true);
         Boolean incSongTitles = sharedPref.getBoolean(SettingsActivity.USE_SONG_TITLE_PREFERENCES, true);
 
+        Boolean matchCompletely = sharedPref.getBoolean(SettingsActivity.USE_COMPLLETE_SEARCH_PREFERENCES, true);
+
+
         List<MatchingMnemonic> filteredArrayNames = new ArrayList<MatchingMnemonic>();
 
         char[] charArray = constraint.toString().toUpperCase().toCharArray();
         Arrays.sort(charArray);
         String searchText = new String(charArray);
+
+        Set<String> strArray = deduplicateTextToSet(searchText);
 
         for (MatchingMnemonic dataNames: originalData) {
             if (dataNames.getDetail().getCategory().equals(MnemonicLoader.SLOGAN_ORIGIN_NAME) && !incSlogans) {
@@ -58,8 +60,28 @@ public class MnemonicFilter extends Filter {
             if (dataNames.getDetail().getCategory().equals(MnemonicLoader.SONG_TITLE_ORIGIN_NAME) && !incSongTitles) {
                 continue;
             }
-            if (dataNames.getOrderedMnemonic().equals(searchText))  {
-                filteredArrayNames.add(dataNames);
+
+            if (matchCompletely) {
+                if (dataNames.getOrderedMnemonic().equals(searchText)) {
+                    filteredArrayNames.add(dataNames);
+                }
+            }
+            else {
+                String mnemonic = dataNames.getMnemonic();
+                if (searchText.length() > mnemonic.length()) {
+                    continue;
+                }
+                int matchCnt = 0;
+                String dedupedMnemonic = deduplicateCharactersFromString(mnemonic.toUpperCase());
+                for (String s : strArray) {
+                   if (dedupedMnemonic.contains(s)) {
+                       matchCnt++;
+                       continue;
+                   }
+                }
+                if (matchCnt == deduplicateCharactersFromString(searchText).length()) {
+                    filteredArrayNames.add(dataNames);
+                }
             }
         }
 
@@ -68,6 +90,23 @@ public class MnemonicFilter extends Filter {
 
         return results;
     }
+
+    private String deduplicateCharactersFromString(String text) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : deduplicateTextToSet(text)) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    private Set<String> deduplicateTextToSet(String text) {
+        Set<String> dedupedSet = new HashSet<String>();
+        for(char c : text.toCharArray()) {
+            dedupedSet.add(c + "");
+        }
+        return dedupedSet;
+    }
+
 
     public List<MatchingMnemonic> getFilteredData() {
         return filteredData;
