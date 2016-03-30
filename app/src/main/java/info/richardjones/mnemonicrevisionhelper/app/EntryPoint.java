009e60e0d1bf348e9191c8a116ad4e957f45db63
iamrichardjones <iamrichardjones@gmail.com>
2016-03-30 20:26:56 +0100

    remove unused imports

--- app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/EntryPoint.java
+++ app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/EntryPoint.java
@@ -1,10 +1,7 @@
 package info.richardjones.mnemonicrevisionhelper.app;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
-import android.preference.PreferenceFragment;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
