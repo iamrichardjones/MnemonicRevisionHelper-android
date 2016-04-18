12445690a66aae1847851e92e0f44f5ff2e7ecfb
iamrichardjones <iamrichardjones@gmail.com>
2016-04-18 13:16:39 +0100

    remove commented out code

--- app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/EntryPoint.java
+++ app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/EntryPoint.java
@@ -17,78 +17,6 @@ import com.google.android.gms.ads.AdView;
 
 public class EntryPoint extends AppCompatActivity {
 
-/*
-    private AdView mAdView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-
-        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
-        // values/strings.xml.
-        mAdView = (AdView) findViewById(R.id.ad_view);
-
-        // Create an ad request. Check your logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-
-        // Start loading the ad in the background.
-        mAdView.loadAd(adRequest);
-    }
-    */
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-     Called when leaving the activity
-        @Override
-    public void onPause() {
-        if (mAdView != null) {
-            mAdView.pause();
-        }
-        super.onPause();
-    }
-
-     Called when returning to the activity
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mAdView != null) {
-            mAdView.resume();
-        }
-    }
-
-    Called before the activity is destroyed
-    @Override
-    public void onDestroy() {
-        if (mAdView != null) {
-            mAdView.destroy();
-        }
-        super.onDestroy();
-    }
-*/
-
-
-
     private AdView mAdView;
 
     @Override
@@ -114,7 +42,6 @@ public class EntryPoint extends AppCompatActivity {
         userInput.addTextChangedListener(new MnemonicInputTextListener(adapter));
     }
 
-
     private List<MatchingMnemonic> loadData() {
         List<MatchingMnemonic> res = new ArrayList();
         new HardCodedMnemonicSloganMapLoader().load(res);
@@ -128,19 +55,14 @@ public class EntryPoint extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_entry_point, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_about) {
             DialogFragment newFragment = new AboutBoxDialogFragment();
             newFragment.show(getSupportFragmentManager(), "ABOUT_KEY");
@@ -156,7 +78,6 @@ public class EntryPoint extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //Called when leaving the activity
     @Override
     public void onPause() {
         if (mAdView != null) {
@@ -165,7 +86,6 @@ public class EntryPoint extends AppCompatActivity {
         super.onPause();
     }
 
-    // Called when returning to the activity
     @Override
     public void onResume() {
         super.onResume();
@@ -174,7 +94,6 @@ public class EntryPoint extends AppCompatActivity {
         }
     }
 
-    // Called before the activity is destroyed
     @Override
     public void onDestroy() {
         if (mAdView != null) {
@@ -182,7 +101,4 @@ public class EntryPoint extends AppCompatActivity {
         }
         super.onDestroy();
     }
-
-
-
 }
