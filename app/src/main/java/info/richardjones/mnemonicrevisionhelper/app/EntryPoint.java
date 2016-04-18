f8a8fb0f48033a224751426b694df79c6e451c3e
iamrichardjones <iamrichardjones@gmail.com>
2016-04-18 13:15:39 +0100

    1_ roll version 2_ add admob support 3_ bring together layout of main page into one file rather than doing it in code

--- app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/EntryPoint.java
+++ app/src/main/java/info/richardjones/mnemonicrevisionhelper/app/EntryPoint.java
@@ -1,37 +1,109 @@
 package info.richardjones.mnemonicrevisionhelper.app;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ListView;
+import android.widget.*;
 import info.richardjones.mnemonicrevisionhelper.app.loader.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class EntryPoint extends AppCompatActivity {
+
+/*
+    private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
+        mAdView = (AdView) findViewById(R.id.ad_view);
+
+        // Create an ad request. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        // Start loading the ad in the background.
+        mAdView.loadAd(adRequest);
+    }
+    */
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+     Called when leaving the activity
+        @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+     Called when returning to the activity
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    Called before the activity is destroyed
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
+*/
+
+
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        LinearLayout layoutMain = new LinearLayout(this);
-        layoutMain.setOrientation(LinearLayout.VERTICAL);
-        setContentView(layoutMain);
+        setContentView(R.layout.main_page);
 
-        LayoutInflater inflate = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        LinearLayout layoutTop = (LinearLayout) inflate.inflate(R.layout.mnemonic_input, null);
-        ListView layoutBottom = (ListView) inflate.inflate(R.layout.results_list, null);
+        mAdView = (AdView) findViewById(R.id.ad_view);
 
-        layoutMain.addView(layoutTop);
-        layoutMain.addView(layoutBottom);
+        AdRequest adRequest = new AdRequest.Builder()
+//                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mAdView.loadAd(adRequest);
 
         final ListView listview = (ListView) findViewById(R.id.listview);
 
@@ -40,9 +112,8 @@ public class EntryPoint extends AppCompatActivity {
 
         final EditText userInput = (EditText) findViewById(R.id.mnemonic_input);
         userInput.addTextChangedListener(new MnemonicInputTextListener(adapter));
-
-
     }
+
 
     private List<MatchingMnemonic> loadData() {
         List<MatchingMnemonic> res = new ArrayList();
@@ -84,6 +155,34 @@ public class EntryPoint extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //Called when leaving the activity
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    // Called when returning to the activity
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    // Called before the activity is destroyed
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
+
 
 
 }
